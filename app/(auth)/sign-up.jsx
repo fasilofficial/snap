a893e4f96e7b6a6859ae07d00c7d3cb37742fe3b
fasilofficial <muhammedfasiloffcial@gmail.com>
2024-04-25 09:04:31 +0530

    Initial setup

---
+++ app/(auth)/sign-up.jsx
@@ -0,0 +1,97 @@
+import { View, Text, ScrollView, Image, Alert } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link, router } from "expo-router";
+import { createUser } from "../../lib/appwrite";
+
+const SignUp = () => {
+  const [form, setForm] = useState({ username: "", email: "", password: "" });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const { setUser, setIsLoggedIn } = useGlobalContext();
+
+  const submit = async () => {
+    if (!form.email || !form.username || !form.password)
+      Alert.alert("Error", "Please fill in all the fields");
+
+    setIsSubmitting(true);
+
+    try {
+      const res = await createUser(form.email, form.password, form.username);
+
+      setUser(res);
+      setIsLoggedIn(true);
+
+      router.push("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115] h-[35]"
+          />
+
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Sign Up
+          </Text>
+
+          <FormField
+            title="Username"
+            value={form.username}
+            placeholder="Enter the username"
+            handleTextChange={(e) => setForm({ ...form, username: e })}
+            otherStyles="mt-7"
+          />
+          <FormField
+            title="Email"
+            value={form.email}
+            placeholder="Enter the email"
+            handleTextChange={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            placeholder="Enter the password"
+            handleTextChange={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Sign Up"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Already have an account?
+            </Text>
+            <Link
+              href="/sign-in"
+              className="text-lg font-pregular text-secondary"
+            >
+              Sign In
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
---
+++ components/SearchInput.jsx
@@ -0,0 +1,46 @@
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import { icons } from "../constants";
+import { router, usePathname } from "expo-router";
+
+const SearchInput = ({ initialQuery }) => {
+  const pathname = usePathname();
+
+  const [query, setQuery] = useState(initialQuery || "");
+
+  return (
+    <View className="flex-row w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center border-2 border-black-200 space-x-4">
+      <TextInput
+        className="text-base mt-0.5 text-white flex-1 font-pregular"
+        value={query}
+        placeholder="Seach for a video topic"
+        placeholderTextColor="#CDCDE0"
+        onChangeText={(e) => setQuery(e)}
+      />
+
+      <TouchableOpacity
+        onPress={() => {
+          if (!query)
+            return Alert.alert(
+              "Missing Query",
+              "Please provide something to search results accross database"
+            );
+
+          if (pathname.startsWith("/search")) router.setParams({ query });
+          else router.push(`/search/${query}`);
+        }}
+      >
+        <Image className="w-5 h-5" resizeMode="contain" source={icons.search} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchInput;
